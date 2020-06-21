e4a1e4d535065750210e9d6f8750b24a917f7fe3
zachmmeyer <zachary.michael.meyer@gmail.com>
2020-06-20 22:33:24 -0400

    Update script.js to allow player to make choice
    It's inside a game function and everything. Made this to move in the bedroom from the laptop mostly.

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-const weapons = {
-    rock: ['scissors', 'lizard'],
-    paper: ['rock', 'spock'],
-    scissors: ['paper', 'lizard'],
-    lizard: ['spock', 'paper'],
-    spock: ['scissors', 'rock'],
-    nuke: ['rock', 'paper', 'scissors', 'lizard', 'spock']
-  }
-  
-  function randomWeapon() {
-    const weaponChoices = Object.keys(weapons);
-    return weaponChoices[Math.floor(Math.random()*weaponChoices.length)];
-  }
-  
-  const playerChoice = randomWeapon();
-  const aiChoice = randomWeapon();
-  
-  console.log(`Player chooses: ${playerChoice}`);
-  console.log(`Computer chooses: ${aiChoice}`);
-  
-  if (aiChoice === playerChoice) {
-    console.log('Draw!');
-  } else {
-    console.log(weapons[aiChoice].includes(playerChoice) ? 'Computer wins!' : 'Player wins!');
-  }
+function game() {
+    const weapons = {
+        rock: ['scissors', 'lizard'],
+        paper: ['rock', 'spock'],
+        scissors: ['paper', 'lizard']
+        //lizard: ['spock', 'paper'],
+        //pock: ['scissors', 'rock'],
+        //nuke: ['rock', 'paper', 'scissors', 'lizard', 'spock']
+    } // Here are the weapons the player and computer can choose from.
+      
+    function randomWeapon() {
+        const weaponChoices = Object.keys(weapons);
+        return weaponChoices[Math.floor(Math.random()*weaponChoices.length)];
+    } // This function picks a weapon from the weapons list.
+    
+    function playerPrompt() {
+        let prompt = window.prompt("Enter 'rock','paper' or 'scissors':");
+        if (!Object.keys(weapons).includes(prompt)) {
+            prompt = window.prompt("Please enter 'rock', 'paper' or 'scissors' only:");
+        }
+        return prompt;
+    } // This function should prompt the player for a weapon choice and store it.
+
+    let playerChoice = playerPrompt(); // Invokes the playerPrompt function for a player to choose a weapon.
+    const aiChoice = randomWeapon(); // Invokes the randomWeapon function for computer to choose a weapon.
+      
+    function determineWinner (){
+        console.log(`Player chooses: ${playerChoice}`);
+        console.log(`Computer chooses: ${aiChoice}`);
+        if (aiChoice === playerChoice) {
+            return 'Draw!';
+            } else {
+            return weapons[aiChoice].includes(playerChoice) ? 'Computer wins!' : 'Player wins!';
+        }
+    } // This function figures out who won.
+
+    return determineWinner();
+}
+
+console.log(game());
